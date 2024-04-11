c819232f234634eb967a6798252a2c01aee6beaa
Yinka09 <kadirimaranatha@yahoo.com>
2024-04-11 01:34:22 -0700

    Fix pycodestyle errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 '''
-Create a function def pascal_triangle(n): that returns a list of lists of integers 
-representing the Pascal's triangle of n:
+Create a function def pascal_triangle(n): that returns
+a list of lists of integers representing the
+Pascal's triangle of n:
 '''
+
 
 def pascal_triangle(n):
     '''
@@ -11,14 +13,14 @@ def pascal_triangle(n):
     '''
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
-    
+
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
         triangle.append(row)
-    
+
     return triangle
